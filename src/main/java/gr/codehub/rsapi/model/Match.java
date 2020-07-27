2a7f2e941e3a02065d94254eb1dff465f66ec10f
Eleni Driva <ele_1924@hotmail.com>
2020-07-27 15:45:56 +0300

    configuration for db

--- src/main/java/gr/codehub/rsapi/model/Match.java
+++ src/main/java/gr/codehub/rsapi/model/Match.java
@@ -6,10 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
@@ -26,6 +23,8 @@ public class Match {
     private Date matchDate;
     private MatchType matchType;
     private Status status;
+    @ManyToOne
     private Applicant applicant;
+    @ManyToOne
     private JobOffer jobOffer;
 }
