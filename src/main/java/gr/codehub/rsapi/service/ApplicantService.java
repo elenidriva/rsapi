c211b0ebef454c96bb07997216fb465443045403
Eleni Driva <ele_1924@hotmail.com>
2020-07-28 07:59:54 +0300

    Completing ApplicantServiceImpl and ApplicantService

--- src/main/java/gr/codehub/rsapi/service/ApplicantService.java
+++ src/main/java/gr/codehub/rsapi/service/ApplicantService.java
@@ -1,6 +1,7 @@
 package gr.codehub.rsapi.service;
 
 import gr.codehub.rsapi.enums.Region;
+import gr.codehub.rsapi.exception.ApplicantNotFoundException;
 import gr.codehub.rsapi.model.Applicant;
 import gr.codehub.rsapi.model.Skill;
 
@@ -11,15 +12,14 @@ public interface ApplicantService {
 
     Applicant addApplicant(Applicant applicant);
 
-    boolean setApplicantStatus(int applicantIndex);  //set inactive
+    boolean setApplicantStatus(int applicantIndex) throws ApplicantNotFoundException;  //set inactive
 
-    Applicant getApplicant(int applicantIndex);
+    Applicant getApplicant(int applicantIndex) throws ApplicantNotFoundException;
 
-    Applicant updateApplicant(Applicant applicant, int applicantIndex);
-// and more
-
+    Applicant updateApplicant(Applicant applicant, int applicantIndex) throws ApplicantNotFoundException;
 
     List<Applicant> getApplicants();
 
-    List<Applicant> getApplicants(Date date, Region region, String firstName, Skill skill);
+    List<Applicant> findApplicantsByCriteria(String lastName, Region region, Date date, Skill skill);
+
 }
--- src/main/java/gr/codehub/rsapi/service/ApplicantServiceImpl.java
+++ src/main/java/gr/codehub/rsapi/service/ApplicantServiceImpl.java
@@ -1,6 +1,8 @@
 package gr.codehub.rsapi.service;
 
 import gr.codehub.rsapi.enums.Region;
+import gr.codehub.rsapi.enums.Status;
+import gr.codehub.rsapi.exception.ApplicantNotFoundException;
 import gr.codehub.rsapi.model.Applicant;
 import gr.codehub.rsapi.model.Skill;
 import gr.codehub.rsapi.repository.ApplicantRepository;
@@ -10,10 +12,17 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.List;
 
+//TODO Dto
 @Service
 public class ApplicantServiceImpl implements ApplicantService {
-    @Autowired
+
     private ApplicantRepository applicantRepository;
+
+    @Autowired
+    public ApplicantServiceImpl(ApplicantRepository applicantRepository) {
+        this.applicantRepository = applicantRepository;
+    }
+
     // constructor
     @Override
     public Applicant addApplicant(Applicant applicant) {
@@ -21,20 +30,33 @@ public class ApplicantServiceImpl implements ApplicantService {
     }
 
     @Override
-    public boolean setApplicantStatus(int applicantIndex) {
-        return false;
+    public boolean setApplicantStatus(int applicantIndex) throws ApplicantNotFoundException {
+        Applicant applicantInDb = applicantRepository.findById(applicantIndex).orElseThrow(() -> new ApplicantNotFoundException("Cannot change Applicant's status, since there is no such Applicant in the DB."));
+
+        if (applicantInDb.getStatus().equals(Status.ACTIVE)) applicantInDb.setStatus(Status.INACTIVE);
+        else applicantInDb.setStatus(Status.ACTIVE);
+
+        applicantRepository.save(applicantInDb);
+        return true;
     }
 
     @Override
-    public Applicant getApplicant(int applicantIndex) {
-        return applicantRepository.findById(applicantIndex).get();
+    public Applicant getApplicant(int applicantIndex) throws ApplicantNotFoundException {
+        return applicantRepository.findById(applicantIndex).orElseThrow(() -> new ApplicantNotFoundException("There is no such Customer in the DB."));
     }
 
     @Override
-    public Applicant updateApplicant(Applicant applicant, int applicantIndex) {
-        Applicant applicantInDb = applicantRepository.findById(applicantIndex).get();//.orElseThrow(() -> new CustomerNotFoundException("Not such customer"));
+    public Applicant updateApplicant(Applicant applicant, int applicantIndex) throws ApplicantNotFoundException {
+        Applicant applicantInDb = applicantRepository.findById(applicantIndex).orElseThrow(() -> new ApplicantNotFoundException("There is no such Applicant."));
+
         applicantInDb.setFirstName(applicant.getFirstName());
         applicantInDb.setLastName(applicant.getLastName());
+        applicantInDb.setStatus(applicant.getStatus());
+        applicantInDb.setAddress(applicant.getAddress());
+        applicantInDb.setDegreeLevel(applicant.getDegreeLevel());
+        applicantInDb.setExperienceLevel(applicant.getExperienceLevel());
+        applicantInDb.setApplicantSkillList(applicant.getApplicantSkillList());
+        applicantInDb.setRegion(applicant.getRegion());
 
         applicantRepository.save(applicantInDb);
 
@@ -43,12 +65,14 @@ public class ApplicantServiceImpl implements ApplicantService {
 
     @Override
     public List<Applicant> getApplicants() {
-       return applicantRepository.findAll();
+        return applicantRepository.findAll();
     }
 
     @Override
-    public List<Applicant> getApplicants(Date date, Region region, String firstName, Skill skill) {
-        return null;
+    public List<Applicant> findApplicantsByCriteria(String lastName, Region region, Date date, Skill skill) {
+
+        return applicantRepository.findApplicantByCriteria(lastName, region, date, skill);
+
     }
 
 }
