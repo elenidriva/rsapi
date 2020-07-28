4df69a84ef2f0df9d9fa876cb82953eec6b0090c
Eleni Driva <ele_1924@hotmail.com>
2020-07-28 14:50:59 +0300

    Adding MatchStatus, SpringFoxConfig, ApplicantDto and ApplicantCreationException

---
+++ src/main/java/gr/codehub/rsapi/configuration/SpringFoxConfig.java
@@ -0,0 +1,28 @@
+package gr.codehub.rsapi.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+/*
+We can see the endpoints through it crm-controller > addCustomer > POST > "john"
+It's a client that is created on its own. It embeds an interface automatically, so we do not need to play with postman
+http://localhost:8080/swagger-ui.html
+ */
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
