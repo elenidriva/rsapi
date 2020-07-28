a155d568842cff6369b281010f5cb00a0ab9a42d
ddouska <dimitraucg93@gmail.com>
2020-07-29 02:01:36 +0300

    Creating Methods findDegreeLevel, in DegreeLevel Enum.

--- src/main/java/gr/codehub/rsapi/enums/DegreeLevel.java
+++ src/main/java/gr/codehub/rsapi/enums/DegreeLevel.java
@@ -1,16 +1,26 @@
 package gr.codehub.rsapi.enums;
 
 public enum DegreeLevel {
-    HIGH_SCHOOL_DIPLOMA(0),
-    ASSOCIATE(1),
-    BACHELOR(2),
-    MASTER(3),
-    PHD(4);
+    HIGH_SCHOOL_DIPLOMA(0, "High School Diploma"),
+    ASSOCIATE(1, "Associate"),
+    BACHELOR(2, "Bachelor"),
+    MASTER(3, "Master"),
+    PHD(4, "Phd");
 
 
     private int value;
+    private String  level;
 
-    DegreeLevel(int value) {
+    DegreeLevel(int value, String level) {
         this.value = value;
+        this.level = level;
+    }
+
+    public static DegreeLevel findDegreeLevel(String level) {
+        for (DegreeLevel degreeLevel : DegreeLevel.values()) {
+            if (degreeLevel.level.equals(level))
+                return degreeLevel;
+        }
+        return DegreeLevel.findDegreeLevel(level);
     }
 }
