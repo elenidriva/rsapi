5263aab477bbe3fd19703aebf29f1f1d48b5691c
ddouska <dimitraucg93@gmail.com>
2020-07-29 02:03:02 +0300

    Creating Methods findRegionByLocation, in Region Enum.

--- src/main/java/gr/codehub/rsapi/enums/Region.java
+++ src/main/java/gr/codehub/rsapi/enums/Region.java
@@ -1,25 +1,34 @@
 package gr.codehub.rsapi.enums;
 
 public enum Region {
-    Attica(0),
-    Central_Greece(1),
-    Central_Macedonia(2),
-    Crete(3),
-    Eastern_Macedonia_and_Thrace(4),
-    Epirus(5),
-    Ionian_Islands(6),
-    North_Aegean(7),
-    Peloponnese(8),
-    South_Aegean(9),
-    Thessaly(10),
-    Western_Greece(11),
-    Western_Macedonia(12);
+    Athens(0,"Athens"),
+    Central_Greece(1,"Central Greece"),
+    Central_Macedonia(2,"Central Macedonia"),
+    Crete(3,"Crete"),
+    Eastern_Macedonia_and_Thrace(4,"Eastern Macedonia_and Thrace"),
+    Epirus(5,"Epirus"),
+    Ionian_Islands(6,"Ionian Islands"),
+    North_Aegean(7,"North Aegean"),
+    Peloponnese(8,"Peloponnese"),
+    South_Aegean(9,""),
+    Thessaly(10,"South Aegean"),
+    Western_Greece(11,"Western Greece"),
+    Western_Macedonia(12,"Western Macedonia");
 
     private int value;
+    private String location;
 
-    Region(int value) {
+    Region(int value, String location) {
         this.value = value;
+        this.location = location;
     }
 
+    public static Region findRegionByLocation(String location) {
+        for (Region region : Region.values()) {
+            if (region.location.equals(location))
+                return region;
+        }
+        return Region.findRegionByLocation(location);
+    }
 
 }
