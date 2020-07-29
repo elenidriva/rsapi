c952d2b32973ea0c4578bd296e6dd8c9869cb68f
Eleni Driva <ele_1924@hotmail.com>
2020-07-29 10:37:09 +0300

    Creating additional exception classes

---
+++ src/main/java/gr/codehub/rsapi/exception/ApplicantIsInactive.java
@@ -0,0 +1,7 @@
+package gr.codehub.rsapi.exception;
+
+public class ApplicantIsInactive extends Throwable {
+    public ApplicantIsInactive(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/gr/codehub/rsapi/exception/JobOfferIsInactive.java
@@ -0,0 +1,7 @@
+package gr.codehub.rsapi.exception;
+
+public class JobOfferIsInactive extends Exception {
+    public JobOfferIsInactive(String message) {
+        super(message);
+    }
+}
